4b592e3bd72e9af1748360b0a600eb516700454d
BooksThere <BooksThere@gmail.com>
2016-01-05 22:30:49 +0900

    refine 02

--- 02.py
+++ 02.py
@@ -3,9 +3,6 @@
 str1 = u"パトカー"
 str2 = u"タクシー"
 
-res = ""
-
-for (a,b) in zip(str1, str2):
-    res += a + b
+res = reduce(lambda l,(a,b): l + a + b, zip(str1, str2),"")
 
 print res.encode('utf-8')
