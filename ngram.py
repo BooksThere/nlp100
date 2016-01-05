7ba26ac8cc47d8c85a8554005b1c343885bab0d7
BooksThere <BooksThere@gmail.com>
2016-01-05 17:08:40 +0900

    refined and fixed ngram.py

--- ngram.py
+++ ngram.py
@@ -1,10 +1,8 @@
 def ngram(seq,n):
     res = []
     length = len(seq)
-    if length < n:
-        return res
-    else:
+    if n <> 0 and length >= n:
         last = length - n
         for i in range(0, last + 1):
             res.append(seq[i:i+n])
-        return res
+    return res
